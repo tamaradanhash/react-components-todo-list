10ae6bcd905dc171b89c01d920619b172a3b93c9
Tamara Danhash <tamaradanhash1989@gmail.com>
2020-03-03 14:33:01 +0100

    Add delete todo button

--- src/Todo.js
+++ src/Todo.js
@@ -11,19 +11,19 @@ export default function Todo({ todo, toggleTodo, deleteTodo }) {
   return (
     <div>
       <button
-        // style={{
-        //   position: 'absolute',
-        //   top: ' 0',
-        //   right: '10px',
-        //   bottom: '0',
-        //   width: '20px',
-        //   height: '20px',
-        //   margin: '0',
-        //   fontSize: ' 15px',
-        //   color: '#cc9a9a',
-        //   marginBottom: '11px',
-        //   transition: ' color 0.2s ease-out',
-        // }}
+        style={{
+          position: 'absolute',
+
+          right: '10px',
+
+          width: '20px',
+          height: '20px',
+          margin: '0',
+          fontSize: ' 15px',
+          color: '#cc9a9a',
+          marginBottom: '11px',
+          transition: ' color 0.2s ease-out',
+        }}
         onClick={handleDeleteTodo}
       >
         x
