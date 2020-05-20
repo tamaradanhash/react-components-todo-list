b1e2cd5cb09b91a0d65038a9dee043e7a4d2ea3d
Tamara Danhash <tamaradanhash1989@gmail.com>
2020-05-20 21:29:49 +0200

    Add setCount to increment the id

--- src/App.js
+++ src/App.js
@@ -8,39 +8,45 @@ function App() {
     name: '',
     completed: false,
   });
+  const [count, setCount] = useState(0);
 
   function handleAddTodo(event) {
     const newTodos = [draft, ...todos];
-    setTodos(newTodos);
     setDraft({
       id: null,
       name: '',
       completed: false,
     });
+    if (draft.name === '') {
+      alert('please enter a todo');
+    } else {
+      setTodos(newTodos);
+    }
     event.preventDefault();
   }
 
   function toggleTodo(id) {
     const newTodos = [...todos];
-    const todo = newTodos.find(todo => todo.id === id);
+    const todo = newTodos.find((todo) => todo.id === id);
     todo.completed = !todo.completed;
     setTodos(newTodos);
   }
   function handleClearTodos() {
-    const newTodos = todos.filter(todo => !todo.completed);
+    const newTodos = todos.filter((todo) => !todo.completed);
     setTodos(newTodos);
   }
   function handleChange(event) {
+    setCount(count + 1);
     const newDraft = {
-      id: todos.length,
+      id: count,
       name: event.target.value,
       completed: false,
     };
+
     setDraft(newDraft);
   }
   function deleteTodo(id) {
-    const newTodos = todos.filter(todo => todo.id !== id);
-
+    const newTodos = todos.filter((todo) => todo.id !== id);
     setTodos(newTodos);
   }
   return (
@@ -59,7 +65,7 @@ function App() {
           toggleTodo={toggleTodo}
           deleteTodo={deleteTodo}
         />
-        <div>{todos.filter(todo => !todo.completed).length} left to do</div>
+        <div>{todos.filter((todo) => !todo.completed).length} left to do</div>
       </form>
     </>
   );
